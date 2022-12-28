5d297adb16ec9ca80a6d2a340188a0d2d486722c
Debdulal Baidya <ovibaidya72@gmail.com>
2022-12-28 20:08:43 +0600

    initial commit

---
+++ src/UtilityServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Elephant\Utility;
+
+use Illuminate\Support\ServiceProvider;
+
+class UtilityServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
