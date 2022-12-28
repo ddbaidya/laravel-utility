bc4065ea752f43695ba6cd9b1597d4caabfff8a1
Debdulal Baidya <ovibaidya72@gmail.com>
2022-12-28 20:34:54 +0600

    Config file publish from UtilityServiceProvider

--- src/UtilityServiceProvider.php
+++ src/UtilityServiceProvider.php
@@ -23,6 +23,8 @@ class UtilityServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        $this->publishes([
+            __DIR__ . '/config/utility.php' => config_path('utility.php'),
+        ]);
     }
 }
