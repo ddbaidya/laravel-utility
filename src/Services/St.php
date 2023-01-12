6d7dfa10f4dc40ca82252a99f7e9467ad9ccea0f
Debdulal Baidya <debdulal.riseuplabs@gmail.com>
2023-01-12 15:48:39 +0600

    withOutString function is included to remove a word from string

--- src/Services/St.php
+++ src/Services/St.php
@@ -23,4 +23,22 @@ trait St
     {
         return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, nibh et bibendum viverra, augue magna congue augue, sit amet aliquam odio est id ex. Sed eget erat euismod, molestie ipsum ut, malesuada ipsum. Curabitur malesuada elit ut augue vehicula, a tincidunt metus iaculis. Curabitur id risus eget eros placerat feugiat. Sed vel velit vel velit tristique euismod. Curabitur id dapibus nisl. Aliquam erat volutpat. Curabitur vitae neque euismod, congue nibh id, interdum velit. Duis vel elit vel nibh euismod fringilla. Sed auctor, nibh et bibendum viverra, augue magna congue augue, sit amet aliquam odio est id ex. Sed eget erat euismod, molestie ipsum ut, malesuada ipsum.";
     }
+
+    /**
+     * Given a string string, remove every occurrence of the string $needle
+     *
+     * @param string $string
+     * @param string $needle
+     * @return string
+     */
+    public static function withOutString($string, $needle)
+    {
+        if (!empty($string) && !empty($needle)) {
+            if (strpos($string, $needle) !== false) {
+                return str_replace($needle, '', $string);
+            }
+        }
+
+        return $string;
+    }
 }
